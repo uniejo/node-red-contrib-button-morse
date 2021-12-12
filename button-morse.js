aa8aacb24c58b0a9326b1864d2b5ea73edac3ce7
Erik Johansen <contrib-morse-20211204@uniejo.dk>
2021-12-12 15:58:21 +0100

    context cleanup

--- button-morse.js
+++ button-morse.js
@@ -11,7 +11,17 @@ module.exports = function(RED) {
         /* --- */
         node.on('start', function(msg) {
 /* --- */
-// Code added here will be run whenever the node is started.
+// Code added here will be run once, when the node is started.
+context.set('timers', [], 'objects');
+context.set('dots', [], 'memory');
+context.set('state', null, 'memory')
+context.set('up_time', (new Date()).getTime(), 'memory');
+context.set('down_time', (new Date()).getTime(), 'memory');
+context.set('up_token_idx', null, 'memory')
+context.set('down_token_idx', null, 'memory')
+context.set('up_token', null, 'memory')
+context.set('down_token', null, 'memory')
+//context.set('dot_length', 333)
 /* --- */
         });
         /* --- */
@@ -23,27 +33,65 @@ module.exports = function(RED) {
         /* --- */
         node.on('input', function(msg) {
 /* --- */
+// Code added here will be run whenever the node receives input.
 /*
 Sends the following messages:
-1) progress, type=key_Fwn (on each line, if enabled)
-2) progress, type=start_time (on eq line, if enabled)
-3) progress, type=end time (on eq line, if enabled)
-4) key_up, delay (on eq line)
-5) progress, type=cancel, (on other lines, if enabled)
+
+1) When button is pressed and released:
+   topic: "button_down", "button_up"    
+   Notitce that debounce should be handled by input, before passing message to this node.
+
+2) While button is pressed, and time passses, a progress message is sent,
+   every time it passes into a new timer action.
+   f.ex.:
+   topic: "progress_short_press", "progress_long_press", "progress_hold", "progress_todo", "progress_quiet", "progress_reboot", "progresss_shutdown", "progress_nop"
+
+3) When key is released, the last progress message is sent as an action.
+   f.ex:
+   topic: "action_short_press", action_quiet", "action_restart", "asction_reboot", "action_nop"
+   
+4) After button is released, progress messages are sent as timer intervals pass.
+   f.ex.:
+   topic: "letter_end", "word_end", "sentence_end", "clear_history"
+   
+5) Next time the button is pressed, the last up message may indicate a token (space) to insert, if history has not been cleared.
+   f.ex.:
+   topic: "action_no_space", action_end_of_character", "action_end_of_word", "action_end_of_sentence"
+   
+The payload may look like this:
+{
+  "dots": "...  ___  ...",     // Notice the use of UTF-8 thin space (\u2009) between morse letters
+  "text": "sos",
+  "sentence": "sos",
+  "word": "sos",
+  "letter": "s"
+}
+
+Note: You may see the following warnings in the debug log:
+    "Unknown context store 'objects' specified. Using default store."
+    "Context ... contains circular referece that cannot be persisted"
+  The warnings have no effect on the funtionality, but you may want to
+  update your .node-red/settings.js to have a contextStorage like this:
+     contextStorage: {
+         default: { module:"localfilesystem" },
+         memory: { module:"memory" },
+         objects: { module:"localfilesystem", config: { cache: true, flushInterval: 28800 } }
+     },
+
+Created by: Erik Johansen, 2021-12-12
+
 */
-const debug = false
+const debug = false //
 const down = 0
 const up = 1
 const states = { [up]: 'up', [down]: 'down' }
-const send_progress = context.get('send_progress') || true;
+const send_progress = context.get('send_progress', 'memory') || true;
 
-const dot_length = context.get('dot_length') || 333
+const dot_length = context.get('dot_length', 'memory') || 333
 const dash_length = dot_length*3
-const letter_pause = dash_length
-const word_pause = dot_length*7
-const sentence_pause = dot_length*12
 
 const thin_space = "\u2009"
+const erase_word = "\u232B"
 
 String.prototype.ucfirst = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
@@ -58,116 +106,99 @@ const down_delays  = [
     {
         time: dash_length/2,   // After 3/2 dot length we accept it as a dash
         token: '_',
-        name: 'long press'
+        name: 'long press',
     },
     {
         time: 3000,
         token: '~' ,
-        name: 'hold'
+        name: 'hold',
+
     },
     {
         time: 7000,
-        name: '!!!',
+        name: 'todo',
+        progress_action: function (t) {
+        },
         action: function (t) {
-            node.warn('!!!')
-    //      send_dot_info('action_xxx', null);
         }
     },
     {
         time: 10000,
         name: 'Quiet',
         progress_action: function (t) {
-            if (debug) node.warn(t.name)
-            send_dot_info('notice_quiet', null);
         },
         action: function (t) {
-            if (debug) node.warn(t.name+'!')
-            send_dot_info('action_quiet', null);
         },
     },
     {
         time: 12500,
         name: 'Reboot',
         progress_action: function (t) {
-            node.warn(t.name)
-            send_dot_info('notice_reboot', null);
         },
         action: function (t) {
-            node.warn(t.name+'!')
-            send_dot_info('action_reboot', null);
         },
     },
     {
         time: 15000,
         name: 'Shutdown',
         progress_action: function (t) {
-            node.warn(t.name)
-            send_dot_info('notice_shutdown', null);
         },
         action: function (t) {
-            node.warn(t.name+'!')
-            send_dot_info('action_shutdown', null);
         },
     },
     {
         time: 20000,
         name: 'NOP',
-        action: function (t) {
-           node.warn(t.name)
-        }
     },
 ];
 
 const up_delays = [
     {
         token: "",
+        name: 'no_space'
     },
     {
-        time: letter_pause,
+        time: dash_length,  // Pause between letters
         token: thin_space,  // Removed after parsing
         name: 'End of letter',
+        progress_label: 'letter_end',
+/*
         progress_action: function(t) {
-            const di = send_dot_info('letter_end', get_dot())
-            if (debug) node.warn(t.name.ucfirst()+': '+di.letter);
-        },  
-        action: function() {
-            // Does not make mutch sense. Is not fired until button is pressed next.
-        }
+        },
+*/
     },
     {
-        time: word_pause,
+        time: dot_length*7,  // Word pause
         token: " ",
         name: 'End of word',
+        progress_label: 'word_end',
+/*
         progress_action: function(t) {
-            const di = send_dot_info('word_end', get_dot())
-            if (debug) node.warn('End of word: '+di.word);
         },
         action: function () {
-            // Does not make mutch sense. Is not fired until button is pressed next.
+            // Does not make much sense. Is not fired until button is pressed next time.
         }
+*/
     },
     {
-        time: sentence_pause,
+        time: dot_length*12, // Sentence pause
         token: "   ",
         name: 'End of sentence',
+        progress_label: 'sentence_end',
         progress_action: function (t) {
-            const di = send_dot_info('sentence_end', get_dot())
-            if (debug) node.warn('End of sentence: '+di.sentence);
+            const history = [].concat(context.get('history', 'memory')||[])
+            history.push(dot_info().text);
+            context.set('history', history, 'memory')
 
-            const history = context.get('history')||[]
-            history.push(di.text);
-            context.set('history', history)
-
-            context.set('dots', null);
+            context.set('dots', null, 'memory');
         }
     },
     {
         time: dot_length*50,
         name: 'Clear history',
+        progress_label: 'clear_history',
         progress_action: function (t) {
-            const di = send_dot_info('clear_history', get_dot())
-            if (debug) node.warn('Clear history');
-            context.set('history', null);
+            context.set('history', null, 'memory');
         }
     },
 
@@ -187,7 +218,7 @@ const button_data = {
 
 const morse_codes = [
       // 8
-      { seq: '........', action: function () { console.log('(TODO: erase last word)') } },
+      { seq: '........', key: erase_word },
       // 6
       { seq: '._._._', key: '.' }, { seq: '__..__', key: ', ' }, { seq: '___...', key: ':' },
       { seq: '..__..', key: '?' }, { seq: '.____.', key: "'"  }, { seq: '_...._', key: '-' }, 
@@ -215,48 +246,44 @@ const morse_codes = [
 
 
 // Clear pending timers
-const timers  = context.get('timers') || [];
+const timers  = context.get('timers', 'objects') || [];
 for (const ct in timers) {
-    if (debug) node.warn('clearTimeout '+ct+' : '+timers[ct]);
+//  if (debug) node.warn('clearTimeout '+ct+' : '+timers[ct]);
     clearTimeout(timers[ct]);
 }
-context.set('timers', []);
+context.set('timers', [], 'objects');
 
 // Get button state (previous and current)
 const state = states[msg.payload]
-const prev_state = context.get('state') || states[1-msg.payload]
+const prev_state = context.get('state', 'memory') || states[1-msg.payload]
 
 // Get data associated with state
 const button = button_data[state];
 const prev_button = button_data[prev_state]
 
-// Calculate time since last press/release
-const last_time  = context.get(prev_state+'_time') || 0;
-let now = (new Date()).getTime();
-let delta_time = now - last_time;
-if (debug) node.warn(prev_state.ucfirst()+' time: '+ delta_time);
-
 // See what token (idx) is selected (shifted by timed events)
-const prev_idx = prev_button && context.get(prev_state+'_token_idx')
-const prev = prev_idx && prev_button.delays[prev_idx]
-const prev_token = prev_button && context.get(prev_state+'_token') || ''
-
-if ( prev && prev.action ) prev.action(prev)
+const prev_idx = prev_button && context.get(prev_state+'_token_idx', 'memory')
+const prev_dly = prev_idx!==null && prev_button.delays[prev_idx]
+const prev_dot = get_dot();
 
 // Add dot (up/down) to list of dots
-const dot = { state: prev_state, time: delta_time, idx: prev_idx, token: prev_token };
-const dots = (context.get('dots') || []);
-if (dot) dots.push(dot);
-context.set('dots', dots);
-context.set('state', state)
-context.set(state+'_time', (new Date()).getTime());
-context.set(state+'_token_idx', 0)
-context.set(state+'_token', button.delays[0].token)
+const dots = (context.get('dots', 'memory') || []);
+if (prev_dot) {
+    if (debug) node.warn(prev_dly.name+'!')
+    const di = send_dot_info(prev_dly.action_label || ('action_'+(prev_dly.name||'no_name').toLowerCase().replace(/[^a-z0-9_]/g, '_')), prev_dot);
+    if (debug) node.warn('Key '+prev_state+' ('+(di.token||'')+')  Word: '+di.word+' Sentence: '+di.sentence+' Dots: '+di.dots + ' Press list: '+JSON.stringify(dots));
+    dots.push(prev_dot);
+}
+if ( prev_dly && prev_dly.action ) prev_dly.action(prev_dly)
+context.set('dots', dots, 'memory');
+context.set('state', state, 'memory')
+context.set(state+'_time', (new Date()).getTime(), 'memory');
+context.set(state+'_token_idx', 0, 'memory')
+context.set(state+'_token', button.delays[0].token || null, 'memory')
 
-
-const di = send_dot_info('button_'+state, null)
-// Note that topic may be for button up, but info is about down state/time (or reverse)
-if (debug) node.warn('Key '+state+' ('+prev_token+')  Word: '+di.word+' Sentence: '+di.sentence+' Dots: '+di.dots + ' Press list: '+( dots && dots.map(p => p.state+':'+p.time+':'+p.token).join('/'))+' - dot: '+JSON.stringify(dot));
+const di = send_dot_info('button_'+state)
+if (debug) node.warn('Key '+state+' ('+(di.token||'')+')  Word: '+di.word+' Sentence: '+di.sentence+' Dots: '+di.dots + ' Press list: '+( dots && dots.map(p => p.state+':'+p.time+':'+p.token).join('/'))+' - dot: '+JSON.stringify(dots[dots.length-1]));
+// Note that topic may be for button up, but info is about previous down state/time (or reverse)
 
 
 // Start new timers
@@ -264,14 +291,17 @@ let new_timers = [];
 button.delays.forEach((act, i) => {
     if ( ! act.time ) return;
 //  node.warn('Start '+state+' timer '+act.time)
-    new_timers.push( setTimeout(function () {
+    const t = setTimeout(function () {
         if (debug) node.warn(state.ucfirst()+' delay '+act.time+' passed ('+(act.name||act.token)+')');
-        context.set(state+'_token', act.token)
-        context.set(state+'_token_idx', i)
-        if ( act.progress_action ) act.progress_action(act)
-    }, act.time));
+        context.set(state+'_token', act.token || null, 'memory')
+        context.set(state+'_token_idx', i, 'memory')
+        if (debug) node.warn(act.name)
+        const di = send_progress && send_dot_info(act.progress_label || ('progress_'+act.name.toLowerCase().replace(/[^a-z0-9_]/g, '_')));
+        if ( act.progress_action ) act.progress_action(act, di)
+    }, act.time);
+    new_timers.push( t );
 });
-context.set('timers', new_timers)
+context.set('timers', new_timers, 'objects')
 
 node.done(); // done with msg
 return null;
@@ -281,17 +311,17 @@ return null;
 **
 */
 function get_dot() {
-    const prev_state = context.get('state');
-    const prev_button = prev_state && button_data[prev_state]
-    if ( !prev_button ) return null;
-    const last_time  = prev_button && context.get(prev_state+'_time') || 0;
+    const state = context.get('state', 'memory') || states[1-msg.payload];
+    const button = state && button_data[state]
+    const idx        = context.get(state+'_token_idx', 'memory')
+    if ( ! ( button && idx!=null && button.delays[idx] ) ) return null;
+    const start_time = context.get(state+'_time', 'memory') || 0;
+    const token      = context.get(state+'_token', 'memory') || ''
     let now = (new Date()).getTime();
-    let delta_time = now - last_time;
-    const prev_idx = prev_button && context.get(prev_state+'_token_idx')
-    const prev = prev_idx && prev_button.delays[prev_idx]
-    const prev_token = prev_button && context.get(prev_state+'_token') || ''
+    let delta_time = now - start_time;
+    if (debug) node.warn(state.ucfirst()+' time: '+ delta_time+' Token: ('+token+')');
 
-    const dot = prev && prev_token && { state: prev_state, time: delta_time, idx: prev_idx, token: prev_token };
+    const dot = { state: state, start: start_time, time: delta_time, idx: idx, token: token };
     return dot;
 }
 /*
@@ -299,9 +329,13 @@ function get_dot() {
 */
 
 function dot_info(dot_temp) {
-    const dots_array = (context.get('dots') || []);
+    const dots_array = [].concat(context.get('dots', 'memory') || []);
+    if ( !dot_temp ) dot_temp=get_dot();
     if ( dot_temp ) dots_array.push(dot_temp);
-    // Here would be a good place to recalculate dot_length, based on actual short/long intervals in dots_array
+    //
+    // Here would be a good place to recalculate context('dot_length'),
+    // based on actual short/long intervals in dots_array.
+    //
     // Join dots into a string
     let dots = dots_array.map(d => d.token || '').join("").replace(/^\s+|\s+$/g, '')
     let match_a = [];
@@ -328,11 +362,12 @@ function dot_info(dot_temp) {
         
     }
 //  node.warn('Processed text: '+text)
-    text = text.replace(/\u2009/g, "")
+    text = text.replace(/\u2009/g, "").replace(/\w*\u232B/g, "")
     const smatch = text.match(/([^\.]+)/g)
     const wmatch = text.match(/(\w+)/g)
     const lmatch = text.match(/(\w)/g)
-    const dot = dots && dots.length && dots[dots.length - 1]
+    const dot = dot_temp || (dots_array && dots_array.length && dots_array[dots_array.length - 1])
+
     const inf = {
         dots:     dots,
         text:     text,
@@ -340,10 +375,12 @@ function dot_info(dot_temp) {
         word:     wmatch && wmatch[wmatch.length - 1],
         letter:   lmatch && lmatch[lmatch.length - 1],
         
-        state:    dot && dot.state,
-        token:    dot && dot.token,
-        time:     dot && dot.time,
+        state:    dot ? dot.state : null,
+        token:    dot ? dot.token : null,
+        time:     dot ? dot.time  : null,
     };    
+    if (debug) node.warn('dot_info from dot: '+JSON.stringify(dot)+'  => '+JSON.stringify(inf));
+
     return inf;
 }
 
@@ -352,7 +389,7 @@ function send_dot_info(topic, dot_temp) {
     const msg = { topic: topic, payload: di };
     node.send(msg)
     return di;
-}    
+}
 /* --- */
         });
         /* --- */
